d117aaaa5424ce899279542f36fb632f44ca3da0
Abhay verma <ramnika.sbi@gmail.com>
2022-02-03 09:38:50 +0530

    initial my-portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:82641-58223">8264158223</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:ramnika.sbi@gmail.com">
+            ramnika.sbi@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Seekho assan tarike se</Slogan>
+        </CompanyContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/constants/constants.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: 'Tailwind Css',
+    description: "I have explanied Tailwind Css all the basic needs you to design and start with it has in parts and it started from part-1 and ended part-6",
+      image: '/images/Coding with abhay.png',
+      tags: ['Html', 'TailwindCss', 'Css'],
+    source: 'https://www.youtube.com/watch?v=WCLlBAg9swg&list=PLFKqtQvZCwu-75tNljCczWVdDHzk7Kt6l',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Bootstrap',
+    description:"In this video i have explained all Fundamentals in 1 Hour And you can make amazing websites with it",
+    image: '/images/99.png',
+    tags: ['Bootstrap', 'Html', 'Web Designing'],
+    source: 'https://www.youtube.com/playlist?list=PLFKqtQvZCwu-GHDCHdAvIWaQzjworclQX',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Javascript Course',
+    description: "In this course i have tell the basics of javascript to master it",
+      image: '/images/javascr thumb.png',
+      tags: ['Javascript', 'Html', 'Course'],
+    source: 'https://www.youtube.com/watch?v=CZED4PJi9sI&list=PLFKqtQvZCwu-071Mcy3rBmFy5E8HCjtE3',
+    visit: 'https://github.com/mystrycoding2009/Javascript-part-1/tree/main',
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: 'Known about youtube', },
+  { year: 2019, text: 'Founded Gaming with Abhay', },
+  { year: 2020, text: 'Learned coding', },
+  { year: 2021, text: 'Uploaded a gaming video', },
+  { year: 2022, text: 'Started teaching coding', },
+];
